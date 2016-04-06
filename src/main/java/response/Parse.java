4ffa8cfc277566405c279d214e53089f4384f983
vishfrnds <vishfrnds@gmail.com>
2016-04-06 15:30:58 +0530

    Recive Post request from Extension

--- src/main/java/response/Parse.java
+++ src/main/java/response/Parse.java
@@ -1,15 +1,36 @@
 package response;
+import org.apache.log4j.Logger;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.BufferUnderflowException;
 
 /**
  * Created by Vishwas on 5/4/16.
  * Copyright (C) 2015  Vishwas Tripathi
  */
 public class Parse {
+    private final static Logger logger = Logger.getLogger(Parse.class);
     public Parse(String html) {
-        final Document parse = Jsoup.parse(html);
+        System.out.println(html);
+        Document parse = Jsoup.parse(html);
+        System.out.println("TEXT\n" + parse.select("body").text());
 
+        logger.debug("title = " );
+    }
+    public static void main (String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String html = "";
+        try {
+            String s;
+            while ((s = br.readLine())!=null){
+                html = s;
+            }
+        }catch (Exception e) {
 
+        }
+        new Parse(html);
     }
 }
